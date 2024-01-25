4003f9ae62b35f1797ff2c1fe9934dbae09088d3
silizard <wkbkmj1@naver.com>
2024-01-25 19:06:47 +0900

    #2.3

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const a = 10;
-const b = 2;
-const myName = "Mjay";
+var a = 10;
+var b = 2;
+var myName = "Mjay"; // var: let과 동일 const처럼 사용 불가(과거의 유일한 변수선언 방식)
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "Mjayee";
+
+console.log("your new name is " + myName);
