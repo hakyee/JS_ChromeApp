84591a025c1eb5d1ae9303c2f5a1155c846a9399
silizard <wkbkmj1@naver.com>
2024-01-25 19:21:33 +0900

    #2.5

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const amIFat = null; // Bolleans: true, false, null, undefined
-let something;
-console.log(amIFat);
-console.log(something);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dayysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+// Get Item from Array
+console.log(dayysOfWeek);
+
+// Add one more day to the array
+dayysOfWeek.push("Non");
+
+console.log(dayysOfWeek);
+
+// const nonsenese = [1, 2, "hello", false, null, true, undefined];
