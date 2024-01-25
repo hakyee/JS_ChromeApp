fcd60e91cdebc44c7fc71abd0e087da6835cdc2a
silizard <wkbkmj1@naver.com>
2024-01-25 19:11:21 +0900

    #2.4

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var a = 10;
-var b = 2;
-var myName = "Mjay"; // var: let과 동일 const처럼 사용 불가(과거의 유일한 변수선언 방식)
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "Mjayee";
-
-console.log("your new name is " + myName);
+const amIFat = null; // Bolleans: true, false, null, undefined
+let something;
+console.log(amIFat);
+console.log(something);
