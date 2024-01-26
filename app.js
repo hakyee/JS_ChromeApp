a353c1f9075d4474f8fe52889bef31c4240a12af
silizard <wkbkmj1@naver.com>
2024-01-26 18:26:33 +0900

    #2.13

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
+const age = parseInt(prompt("How old are you?"));
+console.log(age);
