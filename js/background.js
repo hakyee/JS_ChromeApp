77210c55e10643cee809471f7c6a321696e3fcaf
silizard <wkbkmj1@naver.com>
2024-01-30 17:21:01 +0900

    #6.1

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+// bgImage.src = `img/${chosenImange}`;
+
+// document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundPosition = "top";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundAttachment = "fixed";
