70158ef2ee69db1e3b71329f214fa2852707f19f
silizard <wkbkmj1@naver.com>
2024-01-31 19:46:34 +0900

    Complete!

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "08921f4d11371827ceed82ac2ad28551";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in ", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${Math.round(
+        data.main.temp
+      )}`;
+    });
 }
 
 function onGeoError() {
